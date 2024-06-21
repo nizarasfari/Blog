d3967160cd60e13e7fb786cd0e55336792af43d6
work <wrok@nizars-mbp-14.mynetworksettings.com>
2024-06-21 13:59:33 -0400

    add simple form input

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,13 +1,28 @@
-import { Component } from '@angular/core';
+import { Component , OnInit} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ReactiveFormsModule, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'client';
+  ngOnInit(): void {
+      return;
+  }
+   
+  form!: FormGroup;
+
+  constructor() {
+    this.form = new FormGroup({});
+  }
+
+  onSubmit() {
+    console.log('submit');
+  }
 }
